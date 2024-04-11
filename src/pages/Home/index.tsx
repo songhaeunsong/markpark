d0e74d9a2fc7cac87a4d10cfd9bb2ce5522f64bb
songhaeunsong <gkdms3469@naver.com>
2024-04-11 21:46:40 +0900

    Design: 홈화면 레이아웃 및 디자인

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,23 @@
+const Home = () => {
+  return (
+    <>
+      <div className="flex">
+        <div className="w-80">
+          <img src="/icon.png" alt="markpark-image" />
+        </div>
+        <div className="flex flex-col">
+          <div className="h-40 mt-20">
+            <div className="w-80">
+              <img src="/logo.png" alt="markpark-logo" />
+            </div>
+            <p className="text-white font-medium mt-4 text-center">
+              주변에 있는 주차장을 바로 확인하세요 !
+            </p>
+          </div>
+          <button className="py-2 px-5">내 주변 주차장 찾기</button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Home;
